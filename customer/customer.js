7895940255f4860e27e293208a5d40e413654568
igorashs <igorashsirbu@gmail.com>
2021-02-24 18:14:22 +0200

    add service as filter

--- customer/customer.js
+++ customer/customer.js
@@ -15,6 +15,21 @@ angular
       facturi.reduce((total, f) => total + f.payment, 0);
   })
 
+  .service('adresa', function () {
+    this.address = function (x) {
+      return Object.values(x).join(' ');
+    };
+  })
+
+  .filter('addressFormat', [
+    'adresa',
+    function (adresa) {
+      return function (x) {
+        return adresa.address(x);
+      };
+    }
+  ])
+
   .controller('customerController', function ($scope, $routeParams, facturi) {
     $scope.customer = $scope.customers.find((c) => c.id === $routeParams.id);
     $scope.id = $routeParams.id;
