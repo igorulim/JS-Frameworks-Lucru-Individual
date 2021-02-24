9379e9b242b1e3b9ccb5257f15051194df64ddca
igorashs <igorashsirbu@gmail.com>
2021-02-24 18:13:57 +0200

    add createCustomer

--- createCustomer/createCustomer.js
+++ createCustomer/createCustomer.js
@@ -1,4 +1,6 @@
 'use strict';
+import { getCustomerId } from '../mock.js';
+import { validateCustomerForm } from '../validator.js';
 
 angular
   .module('app')
@@ -11,5 +13,43 @@ angular
   })
 
   .controller('createCustomerController', function ($scope) {
-    console.log('createCustomer controller');
+    $scope.handleSubmit = () => {
+      const {
+        name,
+        personalAccount,
+        postalCode,
+        street,
+        streetNumber
+      } = $scope;
+
+      $scope.formErrors = validateCustomerForm(
+        {
+          name,
+          personalAccount
+        },
+        $scope.customers
+      );
+
+      if (!$scope.formErrors.hasErrors) {
+        $scope.customers.push({
+          id: getCustomerId(),
+          name,
+          personalAccount,
+          address: {
+            postalCode,
+            street,
+            nr: streetNumber
+          },
+
+          invoices: []
+        });
+
+        $scope.result = 'Consumatorul a fost adăugat!';
+      }
+    };
+
+    $scope.handleReset = () => {
+      $scope.formErrors = { hasErrors: false };
+      $scope.result = '';
+    };
   });
